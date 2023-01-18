0df5a6d74366105ac855f013d531c485d8c0d290
foxit64 <foxit64@users.noreply.github.com>
2023-01-18 16:44:04 +0100

    add arg version, code cleanup

--- main.go
+++ main.go
@@ -10,10 +10,15 @@ import (
 	"github.com/akamensky/argparse"
 )
 
-const lowerChars = "abcdefghijklmnopqrstuvwxyz"
-const upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const numberChars = "1234567890"
-const specialChars = "!@#$%^&*()+"
+const (
+	lowerChars   string = "abcdefghijklmnopqrstuvwxyz"
+	upperChars   string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	numberChars  string = "1234567890"
+	specialChars string = "!@#$%^&*()+"
+	versionMajor int    = 0
+	versionMinor int    = 3
+	versionPatch int    = 0
+)
 
 func RandStringBytes(n int, chars string) string {
 	rsb := make([]byte, n)
@@ -31,17 +36,23 @@ func main() {
 	parser := argparse.NewParser("my-gopass", "simple password generator written in GO")
 
 	// creating arguments
-	argLength := parser.Int("l", "length", &argparse.Options{Required: false, Default: 32, Help: "Password length"})
-	argCount := parser.Int("c", "count", &argparse.Options{Required: false, Default: 4, Help: "Password count"})
-	argNumbers := parser.Flag("n", "numbers", &argparse.Options{Required: false, Default: false, Help: "Use number chars"})
-	argSpecial := parser.Flag("s", "special", &argparse.Options{Required: false, Default: false, Help: "Use special chars"})
-	argLower := parser.Flag("o", "lower", &argparse.Options{Required: false, Default: false, Help: "Use only lower chars"})
-	argUpper := parser.Flag("u", "upper", &argparse.Options{Required: false, Default: false, Help: "Use only UPPER chars"})
+	argLength := parser.Int("l", "length", &argparse.Options{Default: 32, Help: "Password length"})
+	argCount := parser.Int("c", "count", &argparse.Options{Default: 4, Help: "Password count"})
+	argNumbers := parser.Flag("n", "numbers", &argparse.Options{Default: false, Help: "Use number chars"})
+	argSpecial := parser.Flag("s", "special", &argparse.Options{Default: false, Help: "Use special chars"})
+	argLower := parser.Flag("o", "lower", &argparse.Options{Default: false, Help: "Use only lower chars"})
+	argUpper := parser.Flag("u", "upper", &argparse.Options{Default: false, Help: "Use only UPPER chars"})
+	argVersion := parser.Flag("", "version", &argparse.Options{Help: "Show version"})
 
 	err := parser.Parse(os.Args)
 	if err != nil {
 		fmt.Print(parser.Usage(err))
 		os.Exit(1)
+	}
+
+	if *argVersion {
+		fmt.Printf("my-gopass v%v.%v.%v", versionMajor, versionMinor, versionPatch)
+		os.Exit(0)
 	}
 
 	// loop count and check args
